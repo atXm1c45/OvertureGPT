0a09aa5fa5ec8f5e8663084879a023e8b615d66c
ATXM <neilmatthewfprudencio@gmail.com>
2025-11-24 14:54:09 +0800

    FINALLY

--- OvertureGPT.py
+++ OvertureGPT.py
@@ -1,10 +1,10 @@
 from google import genai
 import os
 
-client = genai.Client(api_key=os.getenv("GENAI_API_KEY"))
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 response = client.models.generate_content(
-    model="models/gemini-3-pro-preview",
+    model="gemini-2.0-flash",
     contents="Explain how AI works in a few words",
 )
 
